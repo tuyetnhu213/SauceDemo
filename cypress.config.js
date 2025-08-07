4bb2e1030ed725edde88388bd526f7c0b3dfb1c8
Như Trần <Nhu.Tran3@mii.com>
2025-08-08 05:22:47 +0800

    add report and record

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    video: true,
+    videoCompression: 32,
+    videoUploadOnPasses: true,
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+        reportDir: 'cypress/reports',
+        overwrite: false,
+        html: false,
+        json: true
     },
-  },
+
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here if needed
+        },
+        specPattern: 'cypress/e2e/**/*.cy.js'    }
 });
